8d0e5a841b923f0e7830cdcac273904236ce5022
fady-albert <fadyalber2009@gmail.com>
2026-08-08 16:38:38 +0300

    Refactor HTML structure and add CSS styles for chess board layout

---
+++ script.js
@@ -0,0 +1,25 @@
+// import data
+const board = document.getElementById('board');
+
+// make the board
+function boardMake() {
+    for(let row = 0; row < 8; row++) {
+        for(let col = 0; col < 8; col++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+
+            if((row + col) % 2 === 0) {
+                square.classList.add('light');
+            } else {
+                square.classList.add('dark');
+            }
+
+            square.dataset.row = row;
+            square.dataset.col = col;
+
+            board.appendChild(square);
+        }
+    }
+}
+
+boardMake()
